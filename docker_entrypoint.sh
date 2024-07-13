6ece23378d0abb953c96607883f38f93079534e7
Remco Ros <remcoros@live.nl>
2024-07-13 14:36:03 +0000

    expose settings, update instructions

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -6,5 +6,18 @@ echo
 yq e '.config' /updater/data/start9/config.yaml > /updater/data/config.json
 
 export PERIOD=$(yq e '.period' /updater/data/start9/config.yaml)
+export LOG_LEVEL=$(yq e '.advanced.LOG_LEVEL' /updater/data/start9/config.yaml)
+export UPDATE_COOLDOWN_PERIOD=$(yq e '.advanced.UPDATE_COOLDOWN_PERIOD' /updater/data/start9/config.yaml)
+export PUBLICIP_FETCHERS=$(yq e '.advanced.PUBLICIP_FETCHERS' /updater/data/start9/config.yaml)
+export PUBLICIP_HTTP_PROVIDERS=$(yq e '.advanced.PUBLICIP_HTTP_PROVIDERS' /updater/data/start9/config.yaml)
+export PUBLICIPV4_HTTP_PROVIDERS=$(yq e '.advanced.PUBLICIPV4_HTTP_PROVIDERS' /updater/data/start9/config.yaml)
+export PUBLICIPV6_HTTP_PROVIDERS=$(yq e '.advanced.PUBLICIPV6_HTTP_PROVIDERS' /updater/data/start9/config.yaml)
+export PUBLICIP_DNS_PROVIDERS=$(yq e '.advanced.PUBLICIP_DNS_PROVIDERS' /updater/data/start9/config.yaml)
+export PUBLICIP_DNS_TIMEOUT=$(yq e '.advanced.PUBLICIP_DNS_TIMEOUT' /updater/data/start9/config.yaml)
+export HTTP_TIMEOUT=$(yq e '.advanced.HTTP_TIMEOUT' /updater/data/start9/config.yaml)
+export RESOLVER_ADDRESS=$(yq e '.advanced.RESOLVER_ADDRESS //""' /updater/data/start9/config.yaml)
+export RESOLVER_TIMEOUT=$(yq e '.advanced.RESOLVER_TIMEOUT' /updater/data/start9/config.yaml)
+export SHOUTRRR_ADDRESSES=$(yq e '.shoutrrr.SHOUTRRR_ADDRESSES //""' /updater/data/start9/config.yaml)
+export SHOUTRRR_DEFAULT_TITLE=$(yq e '.shoutrrr.SHOUTRRR_DEFAULT_TITLE' /updater/data/start9/config.yaml)
 
 /updater/ddns-updater
--- instructions.md
+++ instructions.md
@@ -1,5 +1,7 @@
 # DDNS Updater instructions
 
-  TODO: write proper instructions
+Configure one or more of the supported DNS providers by pasting the correct JSON string into the 'DDNS Provider(s)' setting.
 
-List of DNS providers and how to configure them: [DDNS Updater provider documentation](https://github.com/qdm12/ddns-updater/tree/master/docs)
+A list of DNS providers and their JSON configuration can be found here: [DDNS Updater provider documentation](https://github.com/qdm12/ddns-updater/tree/master/docs)
+
+Once configured, start the service and inspect the logs to verify that it started successfully and that the DNS updates were successful.
